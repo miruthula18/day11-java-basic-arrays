4fb1ec8bd800f69ae714477ca9e45c5e5785a9cb
miruthula <miruthula183@gmail.com>
2026-07-13 23:42:32 -0700

    Add program to find first occurrence of an element

---
+++ firstoccurance.java
@@ -0,0 +1,17 @@
+public class firstoccurance{
+public static void main(String[] args){
+    int[] arr={10,20,30,10,50};
+    int target=10;
+    int index=-1;
+    for(int i=0;i<arr.length;i++){
+        if(arr[i]==target){
+            index=i;
+            break;
+        }
+
+    }
+    System.out.print(index);
+
+
+    }
+}
