f2f4445e51a3176844a0da86f42259884b1b62f1
miruthula <miruthula183@gmail.com>
2026-07-13 23:44:30 -0700

    Add program to count positive numbers in an array

---
+++ countpositive.java
@@ -0,0 +1,13 @@
+public class countpositive {
+    public static void main(String[] args){
+        int[] arr={10,-58,78,-7,52};
+        int count=0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>0){
+                count++;
+            }
+        }
+        System.out.print(count);
+    }
+    
+}
